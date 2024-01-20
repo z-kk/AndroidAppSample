367bb2d4b09c6909c62757a089ca84b653e3c7b5
z-kk <z-kk@outlook.jp>
2024-01-20 10:36:07 +0900

    MainFragment

---
+++ app/src/main/java/com/example/androidappsample/ui/main/MainFragment.kt
@@ -0,0 +1,29 @@
+package com.example.androidappsample.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.androidappsample.R
+
+class MainFragment : Fragment() {
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
